05dd4c597b26af3013d2ca377665dcec4da087d0
hogesuke <miyado@gmail.com>
2018-10-05 00:03:38 +0900

    expressを使ったfunctionsの設定をお試し

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,13 @@
+const express = require('express');
 const functions = require('firebase-functions');
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+// expressを使用した場合
+const app = express();
+
+app.get('/:id', (req, res) => res.send('Hello!!!'));
+
+exports.trial = functions.https.onRequest(app);
